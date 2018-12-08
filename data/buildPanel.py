f4a7a0e8d553a2fe1d21fab7a4857cb74046acd1
ethancarpenter <ethancarpenter00@comcast.net>
2018-12-07 22:18:54 -0500

    -

--- data/buildPanel.py
+++ data/buildPanel.py
@@ -4,27 +4,28 @@ from datetime import datetime
 from iexfinance.stocks import get_historical_data
 import json
 
-def getCorrelations(tickData, otherData, dayShift, shiftFactor=1, numDecimals=2):
+def getCorrelations(data, otherData, dayShift, shiftFactor=1, limitDecimals=False):
     correlations = []
-    tickDF = pd.DataFrame(tickData)
+    df = pd.DataFrame(data)
     otherDF = pd.DataFrame(otherData)
-    df = tickDF.join(otherDF)
+    df = df.join(otherDF)
 
-    tick = df.columns[0]
+    name = df.columns[0]
     
     for shift in range(dayShift+1):
         shift *= shiftFactor
         dfCopy = df.copy(deep=False)
-        dfCopy[tick] = dfCopy[tick].shift(shift)
+        dfCopy[name] = dfCopy[name].shift(shift)
         corr = df.corr()
         corr = corr.iloc[1, 0]
-        corr = '{0:.3}'.format(corr)
+        if limitDecimals:
+            corr = '{0:.3}'.format(corr)
         correlations.append(corr)
     return correlations
 
-def getClose(ticker, start=datetime(2017, 1, 1), end=datetime.now()):
+def getData(ticker, start=datetime(2017, 1, 1), end=datetime.now(), type='close'):
     data = get_historical_data(ticker, start, end, output_format='pandas')
-    data = data['close']
+    data = data[type]
     data.name = ticker
     return data
 
@@ -34,17 +35,15 @@ def loadTickers():
         #tickers = json.load(file)[0:10]
     return tickers
 
-
-
 def performAnalysis(writeData=False):
     masterCorrelationMap = {}
 
     for tick in tickers:
-        tickData = getClose(tick, start, end)
+        tickData = getData(tick, start, end)
         masterCorrelationMap[tick] = {}
         for otherTick in tickers:
-            if otherTick is not tick:
-                otherData = getClose(otherTick, start, end)
+            if otherTick is not getData:
+                otherData = getData(otherTick, start, end)
                 correlations = getCorrelations(tickData, otherData, dayShift)
                 masterCorrelationMap[tick][otherTick] = correlations
                 
--- viz/dashViz.py
+++ viz/dashViz.py
@@ -2,11 +2,12 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-from data.buildPanel import getClose, getCorrelations
+from data.buildPanel import getData, getCorrelations
 from datetime import datetime
 
 app = dash.Dash()
 app.config['suppress_callback_exceptions'] = True
+
 graph = html.Div(id='graph')
 input1 = html.Div(children=[
     html.Div('Ticker to Predict'),
@@ -20,6 +21,7 @@ dayInput = html.Div(children=[
     html.Div('Max Day Shift'),
     dcc.Input(id='dayShift', value='', type='number')
     ])
+
 children = input1, input2, dayInput, graph
 app.layout = html.Div(children=children)
     
@@ -31,8 +33,8 @@ app.layout = html.Div(children=children)
      Input(component_id='dayShift', component_property='value')])
 def updateGraph(ticker1, ticker2, dayShift):
     try:
-        tickData1 = getClose(ticker1, start=datetime(2014, 1, 1))
-        tickData2 = getClose(ticker2, start=datetime(2014, 1, 1))
+        tickData1 = getData(ticker1, start=datetime(2014, 1, 1))
+        tickData2 = getData(ticker2, start=datetime(2014, 1, 1))
         corr = getCorrelations(tickData1, tickData2, dayShift)
         
         line = {'x': list(range(dayShift+1)), 
@@ -40,14 +42,13 @@ def updateGraph(ticker1, ticker2, dayShift):
                 'name': 'How {} predicts {}'.format(ticker2, ticker1), 
                 'type': 'line'}
         
-        
-        layout = {'title': 'How {} predicts {}'.format(ticker2, ticker1),
-                  'xaxis': {'dtick': 1, 'title': '{} Date minus {} Date'.format(ticker1, ticker2)},
+        layout = {'title': 'How {} predicts {}'.format(ticker2.upper(), ticker1.upper()),
+                  'xaxis': {'dtick': 1, 'title': '{} Date minus {} Date'.format(ticker1.upper(), ticker2.upper())},
                   'yaxis': {'title': 'Correlation Coefficient'}}
         
         return dcc.Graph(id='stock', figure={'data': [line], 'layout': layout})
     except:
-        pass
+        return html.Div("Please enter valid tickers")
     
 if __name__ == '__main__':
     app.run_server(debug=True)
